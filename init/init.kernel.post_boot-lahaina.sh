20bb328fb792f9980501839c30a86faf109197ef
Jake Weinstein <jake@aospa.co>
2022-08-09 07:13:07 +0900

    martini: init: Do not allow restricted tasks to run on big cores
    The restricted cpuset is for system tasks that are
    throttled because the screen is off. Google only
    runs these tasks on the little cluster
    to save power and we will follow suit.
    
    Change-Id: I0174beb11dae2109df4ddf65304bd50c5cc05264

--- init/init.kernel.post_boot-lahaina.sh
+++ init/init.kernel.post_boot-lahaina.sh
@@ -298,6 +298,7 @@ echo 325 > /proc/sys/kernel/walt_low_latency_task_threshold
 echo 0-6 > /dev/cpuset/foreground/cpus
 echo 0-1 > /dev/cpuset/background/cpus
 echo 0-3 > /dev/cpuset/system-background/cpus
+echo 0-3 > /dev/cpuset/restricted/cpus
 
 #cpu cgroup
 echo 3 > /dev/cpuctl/background/cpu.uclamp.window_policy
