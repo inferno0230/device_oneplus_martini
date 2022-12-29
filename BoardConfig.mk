c65f64a3e1f53e454d7c78503d08227166e025e8
inferno0230 <adityachoudhury0230@gmail.com>
2022-12-29 16:50:17 +0530

    Revert "sm8350-common: Allowed neverallows temporarily"
    This reverts commit aebf2e01dabb9d122dd930a400e08f88739a4af3.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -186,7 +186,6 @@ include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
-SELINUX_IGNORE_NEVERALLOWS := true
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
