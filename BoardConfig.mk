10f2ae7297840a66808d9acee1a58d36b1e7be64
Michael Bestas <mkbestas@lineageos.org>
2023-03-03 23:43:42 +0200

    martini: Build Lineage Health HAL

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -126,6 +126,9 @@ BOARD_VENDOR_KERNEL_MODULES_BLOCKLIST_FILE := $(DEVICE_PATH)/modules.blocklist
 BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load))
 TARGET_MODULE_ALIASES += wlan.ko:qca_cld3_wlan.ko
 
+# Lineage Health
+TARGET_HEALTH_CHARGING_CONTROL_CHARGING_PATH := /sys/class/oplus_chg/battery/mmi_charging_enable
+
 # Platform
 BOARD_USES_QCOM_HARDWARE := true
 TARGET_BOARD_PLATFORM := lahaina
