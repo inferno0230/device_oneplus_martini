5d9cc143d2d7af6399db83e1f78df6d8005e5137
inferno0230 <adityachoudhury0230@gmail.com>
2023-04-11 22:53:34 +0530

    martini: Enable OIS
    Co-authored-by: chandu078 <chandudyavanapelli03@gmail.com>

--- device.mk
+++ device.mk
@@ -136,6 +136,7 @@ TARGET_SCREEN_WIDTH := 1080
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
+    libcamera2ndk_vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
 PRODUCT_COPY_FILES += \
