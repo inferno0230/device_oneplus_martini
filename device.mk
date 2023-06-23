857582a2c49bdda0db316b076d7f1b286de72c72
pjgowtham <pjgowtham@gmail.com>
2023-06-23 21:58:32 +0530

    martini: Build oplus camera
    - Camera repository: https://gitlab.com/inferno0230/proprietary_vendor_oplus_camera

--- device.mk
+++ device.mk
@@ -134,6 +134,7 @@ TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
 # Camera
+$(call inherit-product-if-exists, vendor/oplus/camera/opluscamera.mk)
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
