aa624f47ded49b54e283538bdab7af311d9da274
inferno0230 <adityachoudhury0230@gmail.com>
2023-10-31 12:54:58 +0530

    Revert "martini: Introduce dolby"
    This reverts commit aff00dad5d7227677ccaf47a707ec6480cc1f433.
    
    * Until we have A14 Boobas

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -80,13 +80,13 @@ TARGET_SURFACEFLINGER_UDFPS_LIB := //$(DEVICE_PATH):libudfps_extension.oplus
 -include vendor/oneplus/firmware/BoardConfigVendor.mk
 
 # HIDL
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/device_framework_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     vendor/aosp/config/device_framework_matrix.xml
-DEVICE_MATRIX_FILE += $(DEVICE_PATH)/compatibility_matrix.xml
-DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
-ODM_MANIFEST_FILES += $(DEVICE_PATH)/manifest_odm.xml
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+ODM_MANIFEST_FILES := $(DEVICE_PATH)/manifest_odm.xml
 
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_oplus
